ea15bd37f877706f31bd70365298bc068ad6a47f
Elizabeth Vargas <liz2102@gmail.com>
2014-11-28 10:07:52 +0000

    Issue #6 Ground plane projection for front and corridor view

--- matlab/Transform_points/save_gp_proj.m
+++ matlab/Transform_points/save_gp_proj.m
@@ -1,4 +1,4 @@
-function [ nt_gp, gt_gp, nt ] = save_gp_proj( nt_path, gt, T )
+function [ nt_gp, gt_gp, nt ] = save_gp_proj( nt_path, gt, T, sname )
 %SAVE_GP_PROJ Summary of this function goes here
 %
 %   nt_path: path of the .txt file that contains the naive tracker output
@@ -13,5 +13,9 @@ function [ nt_gp, gt_gp, nt ] = save_gp_proj( nt_path, gt, T )
     gt_gp = gt;
     [gt_gp(:,:,1) gt_gp(:,:,2)] = tformfwd(T,gt(:,:,1),gt(:,:,2));
 
+    nt_save = [nt(:,1:6),  nt_gp];
+    
+    csvwrite(sname,nt_save);
+    
 end
 
